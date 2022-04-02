38bfc0abfddadb069590775a769ae37311018333
Pratik Singhal <ps06756@gmail.com>
2022-04-02 08:43:23 +0530

    Added application.properties

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -19,8 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
-
 
 @RestController
 public class HelloController {
